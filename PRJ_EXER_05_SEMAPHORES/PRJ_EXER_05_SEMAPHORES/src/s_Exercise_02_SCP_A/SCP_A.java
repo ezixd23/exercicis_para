94d65bbc101c69e6243a8204ee168bbabe5923fc
zelkarrabi <ziad.elkarrabi@gmail.com>
2024-02-11 17:56:50 +0100

    scp_A

--- PRJ_EXER_05_SEMAPHORES/PRJ_EXER_05_SEMAPHORES/src/s_Exercise_02_SCP_A/SCP_A.java
+++ PRJ_EXER_05_SEMAPHORES/PRJ_EXER_05_SEMAPHORES/src/s_Exercise_02_SCP_A/SCP_A.java
@@ -8,12 +8,14 @@ public class SCP_A {
     public static void main (String [] args) {
         
         /* COMPLETE */
+    	Semaphore canCount = new Semaphore(1);
+    	Semaphore canPrint =  new Semaphore(0);
         Printer [] printers = new Printer[10];
         Storage storage = new Storage();
-        Counter counter = new Counter(storage, /* COMPLETE */);
+        Counter counter = new Counter(storage,canCount, canPrint /* COMPLETE */);
         
         for (int i=0; i<printers.length; i++) {
-        	printers[i] = new Printer(i, storage, /* COMPLETE */);
+        	printers[i] = new Printer(i, storage, canPrint, canCount/* COMPLETE */);
         	printers[i].start();
         }
         
@@ -43,18 +45,24 @@ class Storage {
 class Counter extends Thread {
 	
 	private Storage storage;
-	
+	private Semaphore canCoount;
+	private Semaphore canPrint;
 	/* COMPLETE */
 	
-	public Counter (Storage storage, /* COMPLETE */) {
+	public Counter (Storage storage,Semaphore canCoount,Semaphore canPrint /* COMPLETE */) {
 		this.storage = storage;
 		/* COMPLETE */
+		this.canCoount = canCoount;
+		this.canPrint = canPrint;
 	}
 	
    public void run () {
 	   while (true) {
 		   for (int i=0; i<=9; i++) {
 			   /* COMPLETE */
+			   canCoount.acquireUninterruptibly();
+			   storage.setValue(i);
+			   canPrint.release();
 		   }
 	   }
    }
@@ -65,17 +73,28 @@ class Printer extends Thread {
 	private int id;
 	
 	/* COMPLETE */
+	private Semaphore canCoount;
+	private Semaphore canPrint;
 	
-	public Printer (int id, Storage storage, /* COMPLETE */) {
+	public Printer (int id, Storage storage,Semaphore canPrint,Semaphore canCoount /* COMPLETE */) {
 		this.storage = storage;
 		this.id = id;
 		/* COMPLETE */
+		this.canPrint = canPrint;
+		this.canCoount = canCoount;
 	}
 	
 	public void run () {
 		int val;
 		while (true) {
 			/* COMPLETE */
+			canPrint.acquireUninterruptibly();
+			val = storage.getValue();
+    		for(int i=0;i<=val;i++) {
+    			System.out.print(" ");
+    		}
+    		System.out.println(val);
+			canCoount.release();
 		}
 	  }
 }
