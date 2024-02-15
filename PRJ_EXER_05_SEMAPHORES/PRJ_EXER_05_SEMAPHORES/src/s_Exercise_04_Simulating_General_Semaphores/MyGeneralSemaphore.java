a0a4303c5be862c8d465d4cde35667fb25b9dce9
zelkarrabi <ziad.elkarrabi@gmail.com>
2024-02-15 17:23:37 +0100

    exercice 4

--- PRJ_EXER_05_SEMAPHORES/PRJ_EXER_05_SEMAPHORES/src/s_Exercise_04_Simulating_General_Semaphores/MyGeneralSemaphore.java
+++ PRJ_EXER_05_SEMAPHORES/PRJ_EXER_05_SEMAPHORES/src/s_Exercise_04_Simulating_General_Semaphores/MyGeneralSemaphore.java
@@ -9,17 +9,42 @@ public class MyGeneralSemaphore extends Object {
     
     public MyGeneralSemaphore (int permits) {
        /* COMPLETE */
+    	this.permits = permits;
+    	mutex = new BinarySemaphore(1);
+    	gate = new BinarySemaphore(0);
     }
     
     public void acquire () { // wait on the semaphore
        /* COMPLETE */
-        
+    	 try {
+			mutex.acquire();
+         permits--;
+         if (permits < 0) {
+             mutex.release();
+             gate.acquire();
+         } else {
+             mutex.release();
+         }
+    	 } catch (InterruptedException e) {
+ 			// TODO Auto-generated catch block
+ 			e.printStackTrace();
+ 		}
     }
     
     public void release () { // release
         
         /* COMPLETE */ 
-        
+    	try {
+			mutex.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        permits++;
+        if (permits <= 0) {
+            gate.release();
+        }
+        mutex.release();
     }
     
 }
